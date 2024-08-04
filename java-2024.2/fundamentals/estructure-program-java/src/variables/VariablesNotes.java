06ad3763ab47a314ac74fb1d97ce137433fb32d3
john5ouza <john5souz4@gmail.com>
2024-08-04 12:52:48 -0300

    Studies updates notes

---
+++ java-2024.2/fundamentals/estructure-program-java/src/variables/VariablesNotes.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class VariablesNotes {
+  public static void main(String[] args) {
+
+    int day = 23;
+    int month = 05;
+    int year = 1993;
+
+    System.out.println(day);
+    System.out.println(month);
+    System.out.println(year);
+
+  }
+}
